486d01775f339b58906ee269b2d7f721fa70def3
Karol Pulawski <karol.j.pulawski@gmail.com>
2018-07-14 17:58:10 +0200

    Workshop1 / SearchPopularWebsiteWords - Method sortPopularWords was created. Split up every words from List lines and return them without any alpanumeric signs.

--- src/pl/coderslab/searchPopularWebsiteWords/Main.java
+++ src/pl/coderslab/searchPopularWebsiteWords/Main.java
@@ -7,8 +7,7 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Main {
 
@@ -23,10 +22,39 @@ public class Main {
         String url = "http://www.onet.pl/";
         String specificExpression = "span.title";
 
-        // display in console - test purpose
-        for(String title : connectDownload(url, specificExpression)){
-            System.out.println(title);
+//        // display in console - test purpose 1
+//        for(String title : connectDownload(url, specificExpression)){
+//            System.out.println(title);
+//        }
+
+//        // display in console - test purpose 2
+//        for(String word : sortPopularWords(connectDownload(url, specificExpression))){
+//            System.out.println(word);
+//        }
+
+    }
+
+    /**
+     * Every word from List lines will be separated from non alphanumeric signs.
+     * @param lines List of lines downloaded from website
+     * @return list of sorted words
+     */
+    private static List<String> sortPopularWords(List<String> lines) {
+
+        List<String> words = new ArrayList<>();
+        for(String line : lines) {
+            line = line.trim();
+            StringTokenizer sToken = new StringTokenizer(line);
+            while(sToken.hasMoreTokens()) {
+                String word = sToken.nextToken();
+                if(word.length() > 3) {
+                    word = word.replaceAll("[:,.\"'<>()?!/]", "");
+                    words.add(word);
+                }
+            }
         }
+        Collections.sort(words);
+        return words;
     }
 
     /**
