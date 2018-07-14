e0d0de53c08c507b848d7d00b20800dbfe6d5871
Karol Pulawski <karol.j.pulawski@gmail.com>
2018-07-14 12:08:27 +0200

    Workshop1 / SearchPopularWebsiteWords - Method createDownload was created. Connect with website, download specific text and return list of titles.

--- src/pl/coderslab/searchPopularWebsiteWords/Main.java
+++ src/pl/coderslab/searchPopularWebsiteWords/Main.java
@@ -1,8 +1,55 @@
 package pl.coderslab.searchPopularWebsiteWords;
 
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-
+        runApp();
     }
+
+    /**
+     * application process
+     */
+    private static void runApp() {
+        String url = "http://www.onet.pl/";
+        String specificExpression = "span.title";
+
+        // display in console - test purpose
+        for(String title : connectDownload(url, specificExpression)){
+            System.out.println(title);
+        }
+    }
+
+    /**
+     * Connect with website (url) and download specific text included in specific html attributes
+     * @param url website address
+     * @param specificExpression specific html attributes
+     * @return list of lines from downloaded text
+     */
+    private static List<String> connectDownload(String url, String specificExpression) {
+        List<String> titles = new ArrayList<>();
+        Connection connect = Jsoup.connect(url);
+        try{
+            Document document = connect.get();
+            Elements links = document.select(specificExpression);
+            for(Element elem : links) {
+                titles.add(elem.text());
+            }
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+
+        return titles;
+    }
+
+
 }
